cce8ab35578722e411b38e391efba0759b0bdd14
João Sallé <joaosalle@nasajon.com.br>
2017-10-04 13:29:08 -0300

    Busca

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,28 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { ProductComponent } from './product/product.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { UserComponent } from './user/user.component';
-import { RegisterComponent } from './user/register/register.component';
-import { LoginComponent } from './user/login/login.component';
+import {HomeComponent} from './home/home.component';
+import {ProductComponent} from './product/product.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {UserComponent} from './user/user.component';
+import {RegisterComponent} from './user/register/register.component';
+import {LoginComponent} from './user/login/login.component';
+import {ProductBuscaComponent} from './busca/busca.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'home',  component: HomeComponent },
-  { path: 'product-detail/:id', component: ProductDetailComponent },
-  { path: 'products', component: ProductComponent },
-  { path: 'users', component: UserComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent},
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'product-detail/:id', component: ProductDetailComponent},
+    {path: 'products', component: ProductComponent},
+    {path: 'users', component: UserComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'busca', component: ProductBuscaComponent},
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,52 @@
 // core
 
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { HttpModule } from '@angular/http';
-import { AppRoutingModule } from './app-routing.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {HttpModule} from '@angular/http';
+import {AppRoutingModule} from './app-routing.module';
 
 // components
 
-import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { HomeComponent } from './home/home.component';
-import { PostTestComponent } from './post-test/post-test.component';   // test only
-import { UserComponent } from './user/user.component';
+import {AppComponent} from './app.component';
+import {ProductComponent} from './product/product.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {HomeComponent} from './home/home.component';
+import {PostTestComponent} from './post-test/post-test.component';   // test only
+import {UserComponent} from './user/user.component';
+import {ProductBuscaComponent} from './busca/busca.component'
 
-import { RegisterComponent } from './user/register/register.component';
-import { LoginComponent } from './user/login/login.component';
+import {RegisterComponent} from './user/register/register.component';
+import {LoginComponent} from './user/login/login.component';
 
 // services
 
-import { PostService } from './post-test/post.service';
-import { ProductService } from './product/product.service';
-import { UserService } from './user/user.service';
-
+import {PostService} from './post-test/post.service';
+import {ProductService} from './product/product.service';
+import {UserService} from './user/user.service';
+import {ProductBuscaService} from './busca/busca.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductComponent,
-    ProductDetailComponent,
-    HomeComponent,
-    PostTestComponent,
-    UserComponent,
-    RegisterComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [ProductService, PostService, UserService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProductComponent,
+        ProductDetailComponent,
+        HomeComponent,
+        PostTestComponent,
+        UserComponent,
+        RegisterComponent,
+        LoginComponent,
+        ProductBuscaComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        AppRoutingModule,
+        FormsModule
+    ],
+    providers: [ProductService, PostService, UserService],
+    bootstrap: [AppComponent]
 })
 
-export class AppModule { }
+export class AppModule {}
