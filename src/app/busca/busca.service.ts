3b2425742c757f78a2a46f26e53cb8046874a8ea
cellovit <marcellov1t@hotmail.com>
2017-10-09 23:52:47 -0300

    heroku4

--- src/app/busca/busca.service.ts
+++ src/app/busca/busca.service.ts
@@ -9,10 +9,9 @@ import {Product} from '../product/product.model';
 @Injectable()
 export class ProductBuscaService {
     constructor(private http: Http) {}
-    
     busca(termo: string): Observable<Product[]> {
         return this.http
-                    .get(`http://localhost:1746/product/${termo}`)
+                    .get(`http://34.208.243.21:5000/product/${termo}`)
                     .map(response => response.json().data as Product[]);
     }
 }
