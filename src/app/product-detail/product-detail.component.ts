fd87f837fedc03bdc5fdcd114d457898af958a18
João Sallé <joaosalle@nasajon.com.br>
2017-11-28 15:21:51 -0200

    commit

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -3,6 +3,7 @@ import { Product } from '../product/product.model';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
 import { ProductService } from '../product/product.service';
+import { CartService } from '../cart/cart.service';
 import 'rxjs/add/operator/switchMap';
 
 @Component({
@@ -18,14 +19,37 @@ export class ProductDetailComponent implements OnInit {
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute,
-    private location: Location
+    private location: Location,
+    private cartService: CartService,
   ) {}
-
+  message = 'false';
   ngOnInit(): void {
     this.route.paramMap
       .switchMap((params: ParamMap) => this.productService.getProduct(params.get('id')))
       .subscribe(product => this.product = product);
   }
+  addToCart(productId: string) {
+    this.cartService.add(productId).then(
+      sucess => { window.scrollTo(0, 0); this.message = 'Produto Adicionado com sucesso !'; console.log('s'); },
+      error => { this.message = 'erro';
+                console.log('erro'); }) ;
+  }
 
+  sendMail(target: string, productId: string) {
+    var email = require('../../../node_modules/emailjs/email');
+    var server = email.server.connect({
+      user:"templatestoregpms",
+      password: "bruxafilhadaputa",
+      host: "smtp.gmail.com",
+      ssl: true
+    });
+
+    server.send({
+      text: "http://localhost:4200/product-detail/" + productId,
+      from: "Template Store <templatestoregpms@gmail.com>",
+      to: target,
+      subject: "teste"
+    })
+  }
 
 }
